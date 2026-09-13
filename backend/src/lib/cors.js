d9a1ba3fb92cd5a3821cde73daa892a31578c142
CiRazDeveloper <cirazgames@gmail.com>
2026-09-13 21:32:54 +0200

    Added ability to build for android and ios

--- backend/src/lib/cors.js
+++ backend/src/lib/cors.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 
 export const getAllowedOrigins = () => [
+    process.env.VITE_SERVER_URL,
     process.env.VITE_URL,
     "http://localhost",
     "capacitor://localhost",
