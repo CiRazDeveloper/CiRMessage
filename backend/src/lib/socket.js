07dd6e299a0563abf8c50e40a7a7239f1da45e72
CiRazDeveloper <cirazgames@gmail.com>
2026-09-23 17:57:16 +0200

    Addded group chat with AI

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -182,7 +182,7 @@ export const initializeSocket = (server) => {
                                 },
                             },
                             {
-                                new: true,
+                                returnDocument: "after",
                             }
                         );
 
