490a1cc973a95e27791878240b0db2df430f40d1
CiRazDeveloper <145051936+CiRazDeveloper@users.noreply.github.com>
2026-09-25 14:12:19 +0200

    Expose group admin management routes

--- backend/src/routes/groups.js
+++ backend/src/routes/groups.js
@@ -7,6 +7,9 @@ import {
     loadGroup,
     getGroupMessages,
     leaveGroup,
+    addGroupMember,
+    removeGroupMember,
+    promoteGroupAdmin,
 } from "../controllers/group/group_controller.js";
 
 const router = express.Router();
@@ -18,5 +21,8 @@ router.get("/", listGroups);
 router.get("/:id", loadGroup);
 router.get("/:id/messages", getGroupMessages);
 router.post("/:id/leave", leaveGroup);
+router.post("/:id/members", addGroupMember);
+router.post("/:id/members/:memberId/remove", removeGroupMember);
+router.post("/:id/admins/:memberId/promote", promoteGroupAdmin);
 
 export default router;
